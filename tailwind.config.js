d0cc3ae1cfdb1bc8913c7486ddd3779a276201c1
Marco <seu_email@exemplo.com>
2026-08-02 18:59:18 -0300

    Fix: usa configuracao padrao do postcss compativel com vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Garanta que esta linha esteja apontando para a pasta 'app'
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
